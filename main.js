ead181901fede5a815a295a1707cf78efa644938
Pesara Amarasekera <amarasep@mcmaster.ca>
2024-05-23 10:19:56 -0400

    Added Road definition

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ canvas.width=200;
 
 const context = canvas.getContext("2d");
 const road= new Road(canvas.width/2,canvas.width*0.9);
-const car= new Car(100,100,30,50);
+const car= new Car(road.getLaneCenter(1),100,30,50);
 
 animate();
 
@@ -12,7 +12,13 @@ function animate(){
     car.update();
 
     canvas.height=window.innerHeight;
+    
+    context.save();
+    context.translate(0,-car.y+canvas.height*0.7);
+
     road.draw(context);
     car.draw(context);
+
+    context.restore();
     requestAnimationFrame(animate);
 }
